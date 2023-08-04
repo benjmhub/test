5808315c16290ad133c1c5775557bad1fae34217
Benjamin Muhire ICISHAHAYO <133455959+benjmhub@users.noreply.github.com>
2023-08-04 04:03:03 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,8 +11,8 @@ const data = [
     describe:
       "This repository contains the code for a React e-commerce shopping cart application, implemented using Redux Toolkit and data fetching with Redux Thunk. The shopping cart uses local storage to save data and display items in the browser.",
     tools: ["React js", "Redux", "Redux Toolkit", "Redux Thunk", "Tailwind CSS"],
-    liveLink: "https://rafiul-tech-alpha.netlify.app/",
-   // frontEndLink:// "https://github.com/",
+    /*liveLink: "https://rafiul-tech-alpha.netlify.app/",
+   // frontEndLink:// "https://github.com/",*/
   },
  
   {
@@ -24,9 +24,9 @@ const data = [
     describe:
       "Pormixa is an innovative project management app that helps users manage their projects with ease. With its powerful CRUD (Create, Read, Update, Delete) operations and intuitive interface, Pormixa allows users to create, update, and delete projects effortlessly. Pormixa uses JWT authentication, which allows only authorized users to access the app's features. ",
     tools: ["React","Express","Mongodb","Context-api","Tailwind CSS", ],
-    liveLink: "https://rafiul-proxima.netlify.app/",
+   /* liveLink: "https://rafiul-proxima.netlify.app/",
    // frontEndLink:// "https://github.com/",
-   // backEndLink://"https://github.com/",
+   // backEndLink://"https://github.com/",*/
    
   },
 
@@ -39,8 +39,8 @@ const data = [
     describe:
       "This is a website about cyberbullying, providing information and resources on the topic. The website aims to raise awareness about cyberbullying, its effects, and ways to prevent and address it. The site includes educational content, tips for staying safe online, and resources for individuals and families affected by cyberbullying.",
     tools: ["React", "Tailwind CSS", "Gsap", ],
-    //liveLink: "https://rafiul-cyberbullying.netlify.app/",
-    //fontEndLink:// "https://github.com/",
+   /* //liveLink: "https://rafiul-cyberbullying.netlify.app/",
+    //fontEndLink:// "https://github.com/",*/
    
   },
 ];
